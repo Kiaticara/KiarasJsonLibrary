e63da98f8f713dc5fe2227463e8f537e63ab1594
YaEnergy <kiano.van.cauter@gmail.com>
2025-02-28 20:41:07 +0100

    Update test example

--- example/example.c
+++ example/example.c
@@ -1,10 +1,29 @@
+#include <stdio.h>
+
 #include "json_node.h"
 
 int main()
 {
-    //currently fails
-    
+    printf("creating null node\n");
+
     struct ki_json_node* node = ki_json_node_create_null();
+    
+    printf("destroying null node...\n");
+
+    ki_json_node_destroy(node);
+
+    printf("creating number node...\n");
+
+    node = ki_json_node_create_from_number(5.2);
+    
+    double* num = ki_json_node_try_get_number(node);
+
+    if (num == NULL)
+        printf("num is NULL!");
+    else
+        printf("number node val: %f\n", (float)*num);
+
+    printf("destroying number node...\n");
 
     ki_json_node_destroy(node);
 
