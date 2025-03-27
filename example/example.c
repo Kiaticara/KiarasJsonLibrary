dc262cbfba73810670e09df80001be3d7af2526e
YaEnergy <kiano.van.cauter@gmail.com>
2025-03-27 21:25:38 +0100

    Update example to test basic string parsing

--- example/example.c
+++ example/example.c
@@ -1,7 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 
 #include "ki_json/json_node.h"
 #include "ki_json/json_object.h"
+
+struct json_reader
+{
+    char* json_string;
+    int length;
+
+    int pos;
+};
+
+static bool char_is_whitespace(char character)
+{
+    return (character == ' ') || (character == '\t') || (character == '\n');
+}
+
+static bool char_is_quote(char character)
+{
+    return (character == '\"') || (character == '\'');
+}
+
+//reads 1 char in json string, returns \0 if end is reached
+static char reader_read_char(struct json_reader* reader)
+{
+    assert(reader);
+
+    if (reader->pos == reader->length)
+        return '\0';
+
+    reader->pos++;
+    return reader->json_string[reader->pos - 1];
+}
+
+//parse next quoted string in json string
+//returns true on success and false if not, outs string and string length (not including null terminator)
+//TODO: add escaping with backslash
+static bool parse_string(struct json_reader* reader, char** string, int* length)
+{
+    assert(reader && string && length);
+    
+    int start_pos = reader->pos;
+    *string = NULL;
+    *length = 0;
+
+    char quote = reader_read_char(reader);
+
+    //invalid string, no start quote
+    if (!char_is_quote(quote))
+        return false;
+
+    //get length of string
+    int string_length = 0;
+    char character = reader_read_char(reader);
+    while (character != quote && character != '\0')
+    {
+        string_length++;
+        character = reader_read_char(reader);
+    }
+
+    //invalid string, must have an end quote that is the same as the start quote
+    if (character != quote)
+        return false;
+
+    //alloc string
+    char* new_string = calloc(string_length + 1, sizeof(char));
+
+    if (new_string == NULL)
+        return false;
+
+    //read in string
+    reader->pos = start_pos + 1;
+    for (int i = 0; i < string_length; i++)
+        new_string[i] = reader_read_char(reader);
+
+    new_string[string_length] = '\0'; //null-terminator
+
+    //skip end quote
+    reader->pos++;
+
+    //out
+    *string = new_string;
+    *length = string_length;
+
+    return true;
+}
 
 int main()
 {
@@ -38,6 +124,20 @@ int main()
     
     printf("destroying json object\n");
     ki_json_object_free(object);
+
+    struct json_reader test_reader = {"\"test lol lol\"\"can't see me yet!\" 'crash jk'", 44, 0};
+
+    char* string = NULL;
+    int length = 0;
+
+    //read four strings, third string will be fail as it has no start quote (just a space)
+    for (int i = 0; i < 4; i++)
+    {
+        if (parse_string(&test_reader, &string, &length))
+            printf("read string %i: %s (length = %i) (pos = %i)\n", i + 1, string, length, test_reader.pos);
+        else
+            printf("read string %i is null (length = %i) (pos = %i)\n", i + 1, length, test_reader.pos);
+    }
 
     return 0;
 }
