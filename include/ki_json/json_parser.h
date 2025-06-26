1c37c347dbd8c6eb502a3a43213178fa4fd43305
Kiara <kiano.van.cauter@gmail.com>
2025-06-26 18:51:53 +0200

    Manual include guard (#16)
    * json: manual include guard instead of pragma
    
    * parser: manual include guard instead of pragma
    
    * generator: manual include guard instead of pragma

--- include/ki_json/json_parser.h
+++ include/ki_json/json_parser.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef KI_JSON_PARSER_H
+#define KI_JSON_PARSER_H
 
 // Functions for parsing a json string to a json tree
 
@@ -14,3 +15,5 @@ struct ki_json_val* ki_json_parse_string(const char* string);
 // Parse no more than n characters of string to a json tree.
 // Returns NULL on fail.
 struct ki_json_val* ki_json_nparse_string(const char* string, size_t n);
+
+#endif //KI_JSON_PARSER_H
