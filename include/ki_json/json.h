665afb53a62e8ed9759c35cd37bd84e4a523c9f3
YaEnergy <kiano.van.cauter@gmail.com>
2025-07-03 22:28:10 +0200

    Move error type enum json.h

--- include/ki_json/json.h
+++ include/ki_json/json.h
@@ -61,6 +61,29 @@ struct ki_json_val
     } value;
 };
 
+enum ki_json_error_type
+{
+    KI_JSON_ERROR_NONE, //no error.
+    KI_JSON_ERROR_UNKNOWN, //uh oh...
+
+    KI_JSON_ERROR_MEMORY, //memory error, for ex.: allocation fail
+
+    KI_JSON_ERROR_UNTERMINATED_STRING, //string value does not end
+    KI_JSON_ERROR_UNTERMINATED_ARRAY, //array does not end
+    KI_JSON_ERROR_UNTERMINATED_OBJECT, //object does not end
+
+    KI_JSON_ERROR_NAME_ALREADY_EXISTS, //name already exists in object
+    KI_JSON_ERROR_NO_NAME_VALUE_SEPARATOR, //separator : does not exist between name & value
+    KI_JSON_ERROR_NO_VALUE, //name of name-value pair in object does have a value
+
+    KI_JSON_ERROR_UNKNOWN_TOKEN, //parser can't resolve type of token (number, string, object, array, null)
+    KI_JSON_ERROR_UNKNOWN_ESCAPE_SEQUENCE,
+
+    KI_JSON_ERROR_TRAILING_COMMA, //Trailing comma in array or object is not supported.
+
+    KI_JSON_ERROR_COPY_FAIL, //Should be reported if occurs.
+};
+
 #pragma endregion
 
 //TODO: add val, object and array is_type functions
